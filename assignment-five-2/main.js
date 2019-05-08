7a93594b8f2265c5102bdc4cc170571a72a4450f
originalang <angeorge200@gmail.com>
2019-05-07 22:01:10 -0400

    Create a class for the TypeAhead functionality, and place it in a different file

--- assignment-five-2/main.js
+++ assignment-five-2/main.js
@@ -1,23 +1,14 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-
-const cities = [];
-
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => cities.push(...data));
-
-function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex);
-    });
-}
-
 const searchInput = document.querySelector('#search-input');
 const suggestions = document.querySelector('#suggestions');
 
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+let a = new TypeAhead(endpoint);
+
+searchInput.addEventListener('keyup', displayMatches);
+
 function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
+    const matchArray = a.findMatches(this.value, ["city", "state"]);
     const html = matchArray.map(place => {
         const regex = new RegExp(this.value, 'gi');
         const cityName = place.city.replace(regex, `<mark>${this.value}</mark>`);
@@ -30,5 +21,3 @@ function displayMatches() {
     }).join('');
     suggestions.innerHTML = html;
 }
-
-searchInput.addEventListener('keyup', displayMatches);
---
+++ assignment-five-2/type-ahead.js
@@ -0,0 +1,22 @@
+class TypeAhead {
+    constructor() {
+        this.data = [];
+        fetch(endpoint)
+            .then(blob => blob.json())
+            .then(data => this.data.push(...data));
+    }
+
+    findMatches(wordToMatch, keyList) {
+        if (this.data.length === 0) return
+        return this.data.filter(dataPoint => {
+            const regex = new RegExp(wordToMatch, 'gi');
+
+            for (let key of keyList) {
+                let match = dataPoint[key].match(regex)
+                if (match) {
+                    return match;
+                }
+            }
+        });
+    }
+}
