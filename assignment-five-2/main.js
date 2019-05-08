a82957df6314cbda40f7990a7763da3631340bbf
originalang <angeorge200@gmail.com>
2019-05-08 00:41:41 -0400

    Add a function that abstracts the search input and type ahead process. added another search box to search through GoT episodes

--- assignment-five-2/main.js
+++ assignment-five-2/main.js
@@ -1,27 +1,82 @@
-const searchInput = document.querySelector('#search-input');
-const suggestions = document.querySelector('#suggestions');
+function search(elemId, d) {
+    const searchElement = document.querySelector(`#${elemId}`);
 
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+    let a = {
+        "typeAhead": new TypeAhead(d),
+        "searchInput": searchElement.querySelector('input'),
+        "suggestions": searchElement.querySelector('ul')
+    };
 
-let a = new TypeAhead(endpoint);
+    return a;
+}
 
-searchInput.addEventListener('keyup', displayMatches);
+// city search
+const citiesEndpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-function displayMatches() {
-    const matchArray = a.findMatches(this.value, ["city", "state"]);
+let d = [];
+
+fetch(citiesEndpoint)
+    .then(blob => blob.json())
+    .then(data => d.push(...data));
+
+let citySearch = search("cities", d);
+
+citySearch["searchInput"].addEventListener('keyup', function () {
+    if (this.value === '') {
+        citySearch["suggestions"].innerHTML = '';
+    }
+
+    if (this.value.length < 3) return
+
+    const matchArray = citySearch["typeAhead"].findMatches(this.value, ["city", "state"]);
+
     const html = matchArray.map(place => {
         const regex = new RegExp(this.value, 'gi');
         const cityName = place.city.replace(regex, `<mark>${this.value}</mark>`);
         const stateName = place.state.replace(regex, `<mark>${this.value}</mark>`);
+
         return `
         <li>
-        <span>${cityName}. ${stateName}</span>
+            <span>${cityName}, ${stateName}</span>
         </li>
         `;
     }).join('');
-    suggestions.innerHTML = html;
 
+    citySearch["suggestions"].innerHTML = html;
+});
+
+// game of thrones search
+const gotEndpoint = 'http://api.tvmaze.com/singlesearch/shows?q=game-of-thrones&embed=episodes';
+
+let gotd = [];
+
+fetch(gotEndpoint)
+    .then(blob => blob.json())
+    .then(data => gotd.push(...data["_embedded"]["episodes"]));
+
+let gotSearch = search("got", gotd);
+
+gotSearch["searchInput"].addEventListener('keyup', function () {
+    console.log(this.value)
     if (this.value === '') {
-        suggestions.innerHTML = '';
+        gotSearch["suggestions"].innerHTML = '';
     }
-}
+
+    if (this.value.length < 3) return
+
+    const matchArray = gotSearch["typeAhead"].findMatches(this.value, ["name"]);
+
+    const html = matchArray.map(episode => {
+        const regex = new RegExp(this.value, 'gi');
+        console.log(episode.name);
+        const episodeName = episode.name.replace(regex, `<mark>${this.value}</mark>`);
+
+        return `
+        <li>
+            <span>${episodeName}, S${episode.season}:${episode.number}</span>
+        </li>
+        `;
+    }).join('');
+
+    gotSearch["suggestions"].innerHTML = html;
+});
