27119103e8ca0a3d6e011a0cd962bbcb14268e47
originalang <angeorge200@gmail.com>
2019-05-11 01:13:14 -0400

    Complete part five of the asignment

--- assignment-five-5/main.js
+++ assignment-five-5/main.js
@@ -1,27 +1,158 @@
-function addShow(name) {
-    let current = JSON.parse(localStorage.getItem("showNames"));
+function endpoint(q) {
+    let showName = q.split(' ').join('-')
+    return `https://api.tvmaze.com/singlesearch/shows?q=${showName}&embed=episodes`
+}
 
-    if (current === null) {
-        localStorage.setItem("showNames", JSON.stringify([name]));
-    } else {
-        current.push(name);
-        localStorage.setItem("showNames", JSON.stringify(current));
+function addShow(name, callback) {
+    fetch(endpoint(name)).then((response) => {
+        if (response.status === 200) {
+            let current = JSON.parse(localStorage.getItem("showNames"));
+
+            if (current === null) {
+
+                localStorage.setItem("showNames", JSON.stringify([name]));
+            } else {
+                current.push(name);
+                localStorage.setItem("showNames", JSON.stringify(current));
+            }
+
+            callback();
+        }
+    });
+}
+
+function removeShow(name, callback) {
+    let currentShows = JSON.parse(localStorage.getItem("showNames"));
+
+    currentShows.splice(currentShows.indexOf(name), 1)
+    localStorage.setItem("showNames", JSON.stringify(currentShows));
+
+    callback();
+}
+
+async function getShows() {
+    let showPromise = [];
+
+    var showNames = JSON.parse(localStorage.getItem("showNames"));
+
+    if (showNames.length === 0) {
+        toggleHelp();
     }
+
+    if (showNames !== null) {
+        for (let show of showNames) {
+            await fetch(endpoint(show))
+                .then(blob => blob.json())
+                .then(data => showPromise.push({ key: show, data: data }));
+        }
+    }
+
+    return showPromise;
 }
 
-function removeShow(name) {
-    let current = JSON.parse(localStorage.getItem("showNames"));
+function displayShows() {
+    getShows().then(showList => {
+        showsContainer.innerHTML = '';
+        for (let show of showList) {
+            if (show.data.image) {
+                showsContainer.innerHTML +=
+                    `<div class="show-regular" data-name="${show.key}" onclick="showDetails(this)" onmouseover="showRemoveButton(this);" onmouseleave="hideRemoveButton(this);">
+                        <span data-name="${show.key}" class="remove-show" onclick="removeShow(this.dataset.name, displayShows)">
+                            <i class="fas fa-times"></i>
+                        </span>
+                        <img src="${show.data.image.medium}" />
+                    </div>`
+            }
+        }
 
-    current.splice(current.indexOf(name), 1);
-    localStorage.setItem("showNames", JSON.stringify(current));
+        currentShowData = showList;
+    });
 }
 
-var selectors = document.querySelector('.selectors');
+let currentShowData = []
 
-var showNames = JSON.parse(localStorage.getItem("showNames"));
+const showsContainer = document.querySelector('.shows');
+displayShows();
 
-if (showNames !== null) {
-    for (show of showNames) {
-        selectors.innerHTML += `<span>${show}</span>`;
+const addShowElement = document.querySelector("#add-show");
+
+const addShowButton = addShowElement.querySelector('span');
+const addShowInput = addShowElement.querySelector('input');
+
+const addIcon = document.querySelector('#add-icon');
+const closeIcon = document.querySelector('#close-icon');
+
+addShowButton.addEventListener('click', function () {
+    if (addShowElement.classList.contains('float-in')) {
+        addShowElement.classList.remove('float-in');
+        addShowElement.classList.add('float-out');
+
+        addIcon.style.display = 'inline-block';
+        closeIcon.style.display = 'none';
+    } else {
+        addShowElement.classList.remove('float-out');
+        addShowElement.classList.add('float-in');
+
+        addIcon.style.display = 'none';
+        closeIcon.style.display = 'inline-block';
+    }
+});
+
+addShowInput.addEventListener('change', function () {
+    addShow(this.value, displayShows);
+    this.value = '';
+});
+
+function showRemoveButton(show) {
+    let sp = show.querySelector('span');
+    sp.style.display = "block";
+}
+
+function hideRemoveButton(show) {
+    let rmButton = show.querySelector('span');
+    rmButton.style.display = "none";
+}
+
+function showDetails(show) {
+    let details = document.querySelector('.details');
+
+    details.querySelector('p').innerHTML = getShowData(show.dataset.name).data.summary.replace(/<(.*?)>/g, '');
+
+    show.classList.remove('show-regular');
+    show.classList.add('show-selected');
+
+    getAllSiblings(show).forEach((sibling) => {
+        sibling.classList.remove('show-selected');
+        sibling.classList.add('show-regular');
+    });
+}
+
+function getShowData(showKey) {
+    return currentShowData.filter((show) => {
+        return show.key === showKey;
+    })[0];
+}
+
+function getAllSiblings(node) {
+    let children = node.parentNode.childNodes;
+
+    let siblings = [];
+
+    children.forEach((child) => {
+        if (child !== node) {
+            siblings.push(child);
+        }
+    });
+
+    return siblings;
+}
+
+function toggleHelp() {
+    const help = document.querySelector('#help');
+
+    if (help.style.display === 'none') {
+        help.style.display = 'block'
+    } else {
+        help.style.display = 'none'
     }
 }
